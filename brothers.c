2fc186af6fed7175e732e0c821e8f00f4502c135
NadavNavon <40052972+NadavNavon@users.noreply.github.com>
2018-06-08 13:36:31 +0300

    Update brothers.c

--- brothers.c
+++ brothers.c
@@ -1,1 +1,25 @@
+/*
+This program takes an array with the ages of 4 brothers.
+If the age is smaller than 35, than turn it to 35. Than print.
+*/
 
+#include <stdio.h>
+
+int main() {
+  
+    int age[] = {30, 36, 30, 42};
+    
+    int i;
+    int j = 0;
+    
+//The ~for~ loop set the iteration conditions.
+    for (i = 0; i < sizeof(age[i]); i++) {
+        j += 1;
+        if (age[i] < 35) {age[i] = 35;}
+     
+   
+        printf("The age of brother no. %d is %d\n", j, age[i]);
+    }
+        
+  return 0;
+}
